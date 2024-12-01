729f65de2bf84b2e28f324cf55cc09b9145441fa
Woldemst <adlerrein@gmail.com>
2024-12-01 20:59:01 +0100

    created onboardung flow functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import { useState } from "react";
 import { UncontolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step One</h1>;
+    <button onClick={() => goToNext({ name: "John Doe" })}>Step 1</button>
+  </>
+);
+
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step Two</h1>;
+    <button onClick={() => goToNext({ age: 54 })}>Step 2</button>
+  </>
+);
+
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step Three</h1>;
+    <button onClick={() => goToNext({ hairColor: "brown" })}>Step 3</button>
+  </>
+);
+
 const App = () => {
-  const StepOne = ({ goToNext }) => (
-    <>
-      <h1>Step One</h1>;<button onClick={goToNext}>Step 1</button>
-    </>
-  );
-
-  const StepTwo = ({ goToNext }) => (
-    <>
-      <h1>Step Two</h1>;<button onClick={goToNext}>Step 2</button>
-    </>
-  );
-
-  const StepThree = ({ goToNext }) => (
-    <>
-      <h1>Step Three</h1>;<button onClick={goToNext}>Step 3</button>
-    </>
-  );
-
   return (
     <>
-      <UncontolledOnboardingFlow>
+      <UncontolledOnboardingFlow
+        onFinish={(data) => {
+          console.log(data);
+          alert("Onboarding complete");
+        }}
+      >
         <StepOne />
         <StepTwo />
         <StepThree />
--- src/UncontrolledOnboardingFlow.js
+++ src/UncontrolledOnboardingFlow.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 
-export const UncontolledOnboardingFlow = ({ children, onfinish }) => {
+export const UncontolledOnboardingFlow = ({ children, onFinish }) => {
   const [onboardingData, setOnboardingData] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const currentChild = React.Children.toArray(children)[currentIndex];
 
-  const goToNext = () => {
-    setCurrentIndex(currentIndex + 1);
+  const goToNext = stepData => {
+    const nextIndex = currentIndex + 1; 
+
+    const updatedData = {
+        ...onboardingData,
+        ...stepData, 
+    }
+
+    console.log(updatedData);
+    
+
+    if (nextIndex < children.length){
+        setCurrentIndex(nextIndex)
+    }else {
+        onFinish(updatedData)
+    }
+    setOnboardingData(updatedData)
   };
 
   if (React.isValidElement(currentChild)) {
